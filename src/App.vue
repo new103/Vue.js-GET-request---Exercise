b28fc6e91adbe17fab96c046757b4c96bb990366
new103 <waligora.t@gmail.com>
2021-02-24 11:32:51 +0100

    '24.02.2021'

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <starWars></starWars>
+</template>
+<script>
+import starWars from "./components/starWars";
+export default {
+  components: {
+    starWars: starWars,
+  },
+};
+</script>
---
+++ src/components/starWars.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Star Wars Planets</h1>
+    <div class="planets" v-for="planet of planets">
+      <h2>{{ planet.name }}</h2>
+      <p>{{ planet.climate }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      planets: [],
+    };
+  },
+  created() {
+    this.$http.get("https://swapi.dev/api/planets").then((data) => {
+      console.log(data);
+      this.planets = data.body.results;
+    });
+  },
+};
+</script>
